804daebee1b21cf264e2442c509c2e9682305f87
Yame <yame@free.fr>
2018-01-11 00:01:49 +0100

    Clean project

--- build/default/production/pic_init.p1.d
+++
@@ -1,5 +0,0 @@
- build/default/production/pic_init.d  \
- build/default/production/pic_init.p1:  \
- pic_init.c  \
-./h/hwprofile.h  \
-./h/pic_init.h 
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=PlayerHCI.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/PlayerHCI.X.production.hex
+CND_ARTIFACT_NAME_default=playerHCI.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/playerHCI.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
 CND_PACKAGE_NAME_default=playerhci.x.tar
 CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/playerhci.x.tar
